ee90482e929b8aff6ddf1c3d5b6a129e0dfddfe4
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-09-21 12:16:16 -0400

    editing api

---
+++ public/js/coinmarketcap.js
@@ -0,0 +1,23 @@
+/* Example in Node.js ES6 using request-promise */
+
+const rp = require('request-promise');
+const requestOptions = {
+  method: 'GET',
+  uri: 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+  qs: {
+    'start': '1',
+    'limit': '5000',
+    'convert': 'USD'
+  },
+  headers: {
+    'X-CMC_PRO_API_KEY': 'f2402813-8608-4197-858a-8f42c42fe9a8'
+  },
+  json: true,
+  gzip: true
+};
+
+rp(requestOptions).then(response => {
+  console.log('API call response:', response);
+}).catch((err) => {
+  console.log('API call error:', err.message);
+});
